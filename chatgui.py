725001fb516377e19db6b3b4fa39bafacd257741
baobao925 <96934641+baobao925@users.noreply.github.com>
2022-06-02 19:41:26 +0700

    Update chatgui.py

--- chatgui.py
+++ chatgui.py
@@ -109,7 +109,6 @@ EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
 
 
 #Place all components on the screen
-base.iconbitmap("logo.ico")
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
 EntryBox.place(x=6, y=401, height=90, width=265)
